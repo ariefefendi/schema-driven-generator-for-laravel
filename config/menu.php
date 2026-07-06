6d7a0afd290161baa97f7815f2f0dffef55d4675
ariefefendi <33373513+ariefefendi@users.noreply.github.com>
2026-07-06 18:44:25 +0700

    Update menu.php

--- config/menu.php
+++ config/menu.php
@@ -13,67 +13,10 @@ return [
         'roles' => ['admin', 'operator'],
         'children' => [
             [
-                'title' => 'Nodes (Graph Points)',
-                'url' => '/nodes',
+                'title' => 'Data Bus',
+                'url' => '/buses',
                 'roles' => ['admin', 'operator']
-            ],
-            [
-                'title' => 'Edges (Graph Connections)',
-                'url' => '/edges',
-                'roles' => ['admin', 'operator']
-            ],
-            // [
-            //     'title' => 'Data Bus',
-            //     'url' => '/buses',
-            //     'roles' => ['admin', 'operator']
-            // ],
-            [
-                'title' => 'Places',
-                'url' => '/places',
-                'roles' => ['admin', 'operator']
-            ],
-            // [
-            //     'title' => 'Destinations',
-            //     'url' => '/destinations',
-            //     'roles' => ['admin', 'operator']
-            // ]
-        ]
-    ],
-    [
-        'title' => 'Route Calculation',
-        'icon' => 'fa fa-route',
-        'url' => '/route-calculation',
-        'roles' => ['admin', 'operator']
-    ],
-    // [
-    //     'title' => 'Bus schedule',
-    //     'icon' => 'fa fa-bus',
-    //     'url' => '/bus-schedules',
-    //     'roles' => ['admin', 'operator','driver','user']
-    // ],
-
-    // [
-    //     'title' => 'Map Visualization',
-    //     'icon' => 'fa fa-map',
-    //     'url' => '/map',
-    //     'roles' => ['admin', 'operator','driver','user']
-    // ],
-
-    [
-        'title' => 'Evaluation/Analysis',
-        'icon' => 'fa fa-chart-line',
-        'roles' => ['admin'],
-        'children' => [
-            [
-                'title' => 'Calculation Logs',
-                'url' => '/calculation-logs',
-                'roles' => ['admin']
-            ],
-            // [
-            //     'title' => 'Performance Testing',
-            //     'url' => '/performance-test',
-            //     'roles' => ['admin']
-            // ]
+            ]
         ]
     ],
 
